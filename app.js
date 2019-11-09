7beae5e8cd639329122bd64a1729cd62e999ef93
Jane Skywalker <janehuizeng@gmail.com>
2019-11-08 16:33:18 -0800

    done

--- app.js
+++ app.js
@@ -19,9 +19,7 @@ let data = [
 ];
 
 let schema = ['name','age','location'];
-
 const table = document.querySelector('.table')
-
 const tr = document.createElement('tr')
 
 const createHeader = (schema) => {
@@ -33,11 +31,44 @@ const createHeader = (schema) => {
 }
 
 createHeader(schema)
-
 table.append(tr)
 
+const insertTable = (filter)=>{
+  for(item of data) {
+    const tr = document.createElement('tr')
+    console.log(item)
+      for(let i=0; i<schema.length; i++) {
 
+        console.log(item[schema[i]])
 
+        if(filter) {
+          if(item.name.includes(filter)) {
+            const td = document.createElement('td')
+            td.innerText = item[schema[i]]
+            tr.append(td)
+          }
+        } else {
 
+        const td = document.createElement('td')
+        td.innerText = item[schema[i]]
+        tr.append(td)
+      }
+      }
+      table.append(tr)
+  }
+}
+
+insertTable()
+
+const input = document.querySelector('#input')
+
+input.addEventListener('keyup', (evt)=>{
+  for(var i = 1;i<table.rows.length;){
+    table.deleteRow(i);
+}
+  console.log(evt.target.value)
+  insertTable(evt.target.value)
+})
+console.log(input)
 
 console.log(table)
